897cd7ffd084bb6b4902761f9bfb0575cbffee9a
mdirusso <32931882+mdirusso@users.noreply.github.com>
2019-12-05 06:02:36 -0500

    Add files via upload

--- examplesTER.ml
+++ examplesTER.ml
@@ -36,27 +36,43 @@ let tNested = TmIf(
 multi_vs_Bigstep tNested;;
 
 
+
+
+
+
+
 # use "TER-Cap 7(noinfo).ml";;
 open Term;;
-
-
-let ctx = ("t", NameBind):: ("f", NameBind):: ("l", NameBind):: ("m", NameBind):: ("n", NameBind):: [];;
 let ctx = [];;
 
-let tru = TmAbs("t", TmAbs("f", TmVar(1, 2)));;
-let fls = TmAbs("t", TmAbs("f", TmVar(0, 2)));;
-let c0 = TmAbs("s", TmAbs("z", TmVar(0, 2)));;
-let c1 = TmAbs("s", TmAbs("z", TmApp(TmVar(1, 2), TmVar(0, 2))));;
+(* iff = λl. λm. λn. l m n    *)
+let iff = TmAbs("l", TmAbs("m", TmAbs("n", TmApp(TmApp(TmVar(2, 3), TmVar(1, 3)), TmVar(0, 3)))));;
+let t = TmApp(iff, tru);;
+let iftrue = TmApp(TmApp(t, c0), c1);;			(* iff tru c0 c1 *)
+let tf = TmApp(iff, fls);;
+let iffalse = TmApp(TmApp(tf, c0), c1);;		(* iff fls c0 c1 *)
+
+let ortf = TmApp(TmApp(orr, tru), fls);;		(* orr tru fls *)
+let orff = TmApp(TmApp(orr, fls), fls);;		(* orr fls fls *)
+
+let andtf = TmApp(TmApp(andd, tru), fls);;		(* andd tru fls *)
+let andtt = TmApp(TmApp(andd, fls), fls);;		(* andd tru tru *)
+
+multi_vs_Bigstep ctx iftrue;;
+multi_vs_Bigstep ctx iffalse;;
+multi_vs_Bigstep ctx ortf;;
+multi_vs_Bigstep ctx orff;;
+multi_vs_Bigstep ctx andtf;;
+multi_vs_Bigstep ctx andtt;;
+
+
+
 
 (*λs. λz. z;
 c1 = λs. λz. s z;*)
 
 (* test = λl. λm. λn. l m n;; *)
-let test = TmAbs("l", TmAbs("m", TmAbs("n", TmApp(TmApp(TmVar(2, 3), TmVar(1, 3)), TmVar(0, 3)))));;
-let t = TmApp(test, tru);;
-let tf = TmApp(test, fls);;
-let t11 = TmApp(TmApp(t, c0), c1);;
-let t22 = TmApp(TmApp(tf, c0), c1);;
+
 
 let ex0 = TmApp(
 			TmAbs("u", TmApp(TmVar(0, 1), TmVar(0, 1))),
@@ -65,13 +81,7 @@ let ex0 = TmApp(
 				TmApp(
 					TmAbs("y", TmApp(TmVar(0, 1), TmVar(0, 1))),
 					TmAbs("z", TmVar(0, 1)))));;
-let ex0 = TmApp(
-			TmAbs("u", TmApp(TmVar(0, 1), TmVar(0, 1))),
-			TmApp(
-				TmAbs("x", TmApp(TmVar(0, 1), TmVar(0, 1))),
-				TmApp(
-					TmAbs("y", TmApp(TmVar(0, 1), TmVar(0, 1))),
-					TmAbs("z", TmVar(0, 1)))));;
+
 
 let a = 4;;
 let b = a = a + b;;
