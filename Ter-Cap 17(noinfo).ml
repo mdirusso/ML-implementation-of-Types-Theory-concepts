da56a4909f4059fdcd6e7cc293d1e4146c31a823
mdirusso <32931882+mdirusso@users.noreply.github.com>
2019-12-06 12:05:30 -0500

    Add files via upload

--- Ter-Cap 17(noinfo).ml
+++ Ter-Cap 17(noinfo).ml
@@ -2,36 +2,60 @@
 
 module Term = 
 
+
+		struct
+
 		type ty =
-		| TyNat
-		| TyBool
-		| TyRecord of (string * ty) list
-		| TyTop
-		| TyArr of ty * ty
+			| TyNat
+			| TyBool
+			| TyRecord of (string * ty) list
+			| TyTop
+			| TyArr of ty * ty
+		
+		
 		
        type term =
-	   
-	   	| TmTrue
-		| TmFalse			
-		| TmZero
-		| TmPred of term
-		| TmSucc of term
-		| TmIsZero of term 
-		| TmIf of term * term * term
-        | TmRecord of (string * term) list
-		| TmProj of  term * string
-		| TmVar of iint * int
-		| TmAbs of string * ty * term
-		| TmApp of  term * term
+			| TmTrue
+			| TmFalse			
+			| TmZero
+			| TmPred of term
+			| TmSucc of term
+			| TmIsZero of term 
+			| TmIf of term * term * term
+			| TmRecord of (string * term) list
+			| TmProj of  term * string
+			| TmVar of int * int
+			| TmAbs of string * ty * term
+			| TmApp of term * term
+		
+		
 		
 		
 		let rec subtype tyS tyT = 
-			(=) tyS tyT || 
-			match (tyS,tyT) with
+			(=) tyS tyT || 																					(*S-REFL*)
+			(match (tyS,tyT) with
 				| (TyRecord(fS), TyRecord(fT)) ->  List.for_all
-							(fun (li,tyTi) ->try let tySi = List.assoc li fS in
-							subtype tySi tyTi  with Not_found -> false)
+							(fun (li,tyTi) ->
+								try let tySi = List.assoc li fS in
+									subtype tySi tyTi  														(*S-RCDDEPTH*)
+								with Not_found -> false)
 							fT
-				| (_,TyTop)  -> true
-				| (TyArr(tyS1,tyS2),TyArr(tyT1,tyT2)) ->  (subtype tyT1 tyS1) && (subtype tyS2 tyT2)
+				| (_,TyTop)  -> true																		(*S-TOP*)
+				| (TyArr(tyS1,tyS2),TyArr(tyT1,tyT2)) ->  (subtype tyT1 tyS1) && (subtype tyS2 tyT2) 		(*S-ARROW*)
 				| (_,_) -> false
+			)
+				
+				
+		
+		
+		
+		
+		
+		
+		
+let a = TmRecord([("n", TmTrue); ("b", TmZero)]);;
+let b = TmRecord([("b", TmZero); ("n", TmTrue)]);;
+subtype a b;
+				
+				
+end;;
